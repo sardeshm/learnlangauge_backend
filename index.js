518ea4e0944259e4f503106801b0b99c4b39ec72
SDeshmukh <sarikasachind@gmail.com>
2021-11-04 15:43:58 +0100

    herokuu

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 //const transporter = require("./middleware/transporter");
 const cors = require("cors");
